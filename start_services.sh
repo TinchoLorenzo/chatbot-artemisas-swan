83555ca5b28a385c8baf6a8082d832c71414ef2d
mlorenzo <mlorenzo@alumnos.exa.unicen.edu.ar>
2020-12-02 11:54:12 -0300

    as

--- start_services.sh
+++ start_services.sh
@@ -1,3 +1,3 @@
 cd app/
 # Start rasa server with nlu model
-rasa run --model models --enable-api --cors "*" --debug -p $PORT
+rasa run --model models --endpoints endpoints.yml --enable-api --cors "*" --debug -p $PORT
